3bc6ba42f4ae0d70ff0d931c950bba9de03eb29a
Saied Gaffer <saied2421998@gmail.com>
2022-09-14 17:51:23 +0200

    finish contact-form

--- src/components/contactSection/contactForm/contactForm.js
+++ src/components/contactSection/contactForm/contactForm.js
@@ -1,24 +1,26 @@
 const ContactForm = () => {
   const endPoint = 'https://public.herotofu.com/v1/08e32500-3408-11ed-9de0-b73c4b901972';
   return (
-    <form className="contact-form mt3 w-70 flex flex-wrap justify-center" action={endPoint} method="post">
+    <form className="contact-form mt3 w-70 flex flex-wrap justify-center" action={endPoint} method="post" target="_blank">
       <input
-        className="w-100 br3 pa3"
+        className="w-100-m w-100 br3 pa3"
         type="text"
         placeholder="Name"
+        name="name"
         style={{ marginRight: 'auto' }}
         required
       />
       <input
-        className="ww-100 br3 pa3"
+        className="w-100-m w-100 br3 pa3"
         type="email"
         placeholder="Email"
+        name="email"
         style={{ marginLeft: 'auto' }}
         required
       />
-      <input className="w-100 br3 pa3" placeholder="Subject" type="text" required />
-      <textarea className="w-100 br3 pa3" placeholder="Message" required />
-      <div className="flex flex-row-ns flex-column justify-center" style={{ gap: '0.5rem 1rem' }}>
+      <input className="w-100 br3 pa3" placeholder="Subject" type="text" name="subject" required />
+      <textarea className="w-100 br3 pa3" name="message" placeholder="Message" required />
+      <div className="flex flex-row-ns flex-column justify-center" style={{ gap: '1rem' }}>
         <input
           className="submit-btn pointer pa2 pl3 pr3 br3 tc b--none center f4 fw6"
           type="submit"
