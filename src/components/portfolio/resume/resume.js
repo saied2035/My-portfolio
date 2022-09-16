bcb7683c39cec94c1962774ac193b1426d60ddb5
Saied Gaffer <saied2421998@gmail.com>
2022-09-16 13:59:29 +0200

    move resume part to the end of the portfolio section

--- src/components/portfolio/resume/resume.js
+++ src/components/portfolio/resume/resume.js
@@ -1,7 +1,7 @@
 const Resume = () => (
   <a
     href="https://drive.google.com/file/d/1laDLHG-ul9H2npETX3v27hjAczO81tFf/view?usp=sharing"
-    className="download-btn pa2 pl4 pr4 br3 tc b--none center f3 fw6"
+    className="download-btn pa2 mb4 pl4 pr4 br3 tc b--none center f3 fw6"
     target="_blank"
     rel="noreferrer"
   >
