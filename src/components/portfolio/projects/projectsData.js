afcce0a4a24c4b7243629a723b10a48267528bd3
Saied Gaffer <saied2421998@gmail.com>
2022-11-11 18:28:20 +0200

    add new project

--- src/components/portfolio/projects/projectsData.js
+++ src/components/portfolio/projects/projectsData.js
@@ -1,19 +1,69 @@
-import flightfullImgPreview from './screenshots/Flightfull/preview.jpg';
-import flightfullPopupImgPreview from './screenshots/Flightfull/popup-preview.jpg';
 import budgetAppImgPreview from './screenshots/Budget-app/preview.jpg';
 import budgetAppPopupImgPreview from './screenshots/Budget-app/popup-preview.jpg';
+import smartBrainImgPreview from './screenshots/Smart-brain/preview.jpg';
+import smartBrainPopupImgPreview from './screenshots/Smart-brain/popup-preview.jpg';
+import flightfullImgPreview from './screenshots/Flightfull/preview.jpg';
+import flightfullPopupImgPreview from './screenshots/Flightfull/popup-preview.jpg';
 import robofriendsImgPreview from './screenshots/Robofriends/preview.jpg';
 import robofriendsPopupImgPreview from './screenshots/Robofriends/popup-preview.jpg';
 import spaceTravellerImgPreview from './screenshots/Space-traveller/preview.jpg';
 import spaceTravellerPopupImgPreview from './screenshots/Space-traveller/popup-preview.jpg';
 import mathMagicianImgPreview from './screenshots/Math-magician/preview.jpg';
 import mathMagicianPopupImgPreview from './screenshots/Math-magician/popup-preview.jpg';
-import movies2WatchImgPreview from './screenshots/Movies2Watch/preview.jpg';
-import movies2WatchPopupImgPreview from './screenshots/Movies2Watch/popup-preview.jpg';
 
 const projects = [
   {
     id: 1,
+    name: 'Budget app',
+    preview: budgetAppImgPreview,
+    technologies: [
+      {
+        name: 'Ruby on Rails',
+        link: 'https://github.com/rails/rails',
+      },
+      {
+        name: 'Devise',
+        link: 'https://github.com/heartcombo/devise',
+      },
+      {
+        name: 'CanCanCan',
+        link: 'https://github.com/CanCanCommunity/cancancan',
+      },
+    ],
+    popupPreview: budgetAppPopupImgPreview,
+    info: 'RoR app that helps users to add their categories and transactions.',
+    demo: 'https://rails-budget-app-production.up.railway.app/',
+    source: 'https://github.com/saied2035/Rails-budget-app',
+  },
+  {
+    id: 2,
+    name: 'Smart Brain',
+    preview: smartBrainImgPreview,
+    technologies: [
+      {
+        name: 'React',
+        link: 'https://github.com/facebook/react',
+      },
+      {
+        name: 'Redux',
+        link: 'https://github.com/reduxjs/redux',
+      },
+      {
+        name: 'NodeJS',
+        link: 'https://github.com/nodejs/node',
+      },
+      {
+        name: 'ExpressJS',
+        link: 'https://github.com/expressjs/express',
+      },
+    ],
+    popupPreview: smartBrainPopupImgPreview,
+    info: 'The Smart Brain Project is a web app that detects faces and guesses their genders, ages, and mood. it got built with React & Redux front-end side and Nodejs & Postgresql back-end side.',
+    demo: 'https://saied2035.github.io/smartbrain/',
+    source: 'https://github.com/saied2035/smartbrain',
+  },
+  {
+    id: 3,
     name: 'Flightfull',
     preview: flightfullImgPreview,
     technologies: [
@@ -48,50 +98,8 @@ const projects = [
     source: 'https://github.com/saied2035/Flightfull-frontend',
   },
   {
-    id: 2,
-    name: 'Budget app',
-    preview: budgetAppImgPreview,
-    technologies: [
-      {
-        name: 'Ruby on Rails',
-        link: 'https://github.com/rails/rails',
-      },
-      {
-        name: 'Devise',
-        link: 'https://github.com/heartcombo/devise',
-      },
-      {
-        name: 'CanCanCan',
-        link: 'https://github.com/CanCanCommunity/cancancan',
-      },
-    ],
-    popupPreview: budgetAppPopupImgPreview,
-    info: 'RoR app that helps users to add their categories and transactions.',
-    demo: 'https://glacial-lake-16247.herokuapp.com/',
-    source: 'https://github.com/saied2035/Rails-budget-app',
-  },
-  {
-    id: 3,
-    name: 'Robofriends',
-    preview: robofriendsImgPreview,
-    technologies: [
-      {
-        name: 'React',
-        link: 'https://github.com/facebook/react',
-      },
-      {
-        name: 'Redux',
-        link: 'https://github.com/reduxjs/redux',
-      },
-    ],
-    popupPreview: robofriendsPopupImgPreview,
-    info: 'Robofriends is a website that displays robots and has an instant search by name. I built the website built by React and Redux.',
-    demo: 'https://saied2035.github.io/robofriends-react/',
-    source: 'https://github.com/saied2035/robofriends-react',
-  },
-  {
     id: 4,
-    name: 'Space traveller',
+    name: 'Space traveler',
     preview: spaceTravellerImgPreview,
     technologies: [
       {
@@ -114,6 +122,25 @@ const projects = [
   },
   {
     id: 5,
+    name: 'Robofriends',
+    preview: robofriendsImgPreview,
+    technologies: [
+      {
+        name: 'React',
+        link: 'https://github.com/facebook/react',
+      },
+      {
+        name: 'Redux',
+        link: 'https://github.com/reduxjs/redux',
+      },
+    ],
+    popupPreview: robofriendsPopupImgPreview,
+    info: 'Robofriends is a website that displays robots and has an instant search by name. I built the website built by React and Redux.',
+    demo: 'https://saied2035.github.io/robofriends-react/',
+    source: 'https://github.com/saied2035/robofriends-react',
+  },
+  {
+    id: 6,
     name: 'Math magician',
     preview: mathMagicianImgPreview,
     technologies: [
@@ -130,29 +157,6 @@ const projects = [
     info: "Calculator website with react router. It's single page application that enables you to perform mathematical operations, read a quote about mathematics and an introduction to math on the home page",
     demo: 'https://math-magic-site.netlify.app/',
     source: 'https://github.com/saied2035/math-magicians-full-website',
-  },
-  {
-    id: 6,
-    name: 'Movies2Watch',
-    preview: movies2WatchImgPreview,
-    technologies: [
-      {
-        name: 'JavaScript/ES6',
-        link: 'https://github.com/lukehoban/es6features',
-      },
-      {
-        name: 'HTML5',
-        link: 'https://www.w3schools.com/html/',
-      },
-      {
-        name: 'CSS3',
-        link: 'https://www.w3schools.com/css/',
-      },
-    ],
-    popupPreview: movies2WatchPopupImgPreview,
-    info: 'Display list of movies and their descriptions with ability of commint, like, and react.',
-    demo: 'https://saied2035.github.io/movie-app/',
-    source: 'https://github.com/saied2035/movie-app',
   },
 ];
 
