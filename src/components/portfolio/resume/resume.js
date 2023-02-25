ab1048b2c126b76d51d686e3f7b05af536fa6adb
Saied Gaffer <saied2421998@gmail.com>
2023-02-25 12:49:03 +0200

    update resume

--- src/components/portfolio/resume/resume.js
+++ src/components/portfolio/resume/resume.js
@@ -1,6 +1,6 @@
 const Resume = () => (
   <a
-    href="https://drive.google.com/file/d/1laDLHG-ul9H2npETX3v27hjAczO81tFf/view?usp=sharing"
+    href="https://drive.google.com/file/d/1iNxE1RvFFVVdGIE7swcvXYSVl70mI_w2/view?usp=sharing"
     className="download-btn pa2 mb4 pl4 pr4 br3 tc b--none center f3 fw6"
     target="_blank"
     rel="noreferrer"
